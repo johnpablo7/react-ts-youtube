9812df706df5df44ae8da88a2ffccc0ce23a852c
john <john.davis.2015.03@gmail.com>
2022-01-16 11:49:22 -0500

    React + TS: useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Usuario } from "./components/Usuario";
 
 import { TimerPadre } from "./components/TimerPadre";
 
+import { ContadorRed } from "./components/ContadorRed";
+
 function App() {
   return (
     <>
@@ -10,14 +12,16 @@ function App() {
       <hr />
 
       <h2>useState</h2>
-
       <Counter />
       <Usuario />
 
       <h2>useEffect - useRef</h2>
       <hr />
-
       <TimerPadre />
+
+      <h2>useReducer</h2>
+      <hr />
+      <ContadorRed />
     </>
   );
 }
